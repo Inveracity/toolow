1d66b7e890c3cd39dfa3033f189e02938dd89247
Christopher Baklid <Inveracity@users.noreply.github.com>
2022-11-08 15:55:40 +0100

    stream ffmpeg output to app (#1)
    * stream ffmpeg output to app
    
    * stream ffmpeg output to frontend
    
    * overflow-x hidden
    
    * move buttons up\nmodularise and update readme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Normalize, Videofile, Volume } from "../wailsjs/go/main/App";
-import { Box, Button, Stack, Text, Image, Progress, Divider } from "@chakra-ui/react"
+import { Box, Button, Stack, Text, Image, Progress, Divider, Collapse } from "@chakra-ui/react"
 import banner from "./assets/banner.png"
-
+import { EventsOff, EventsOn } from "../wailsjs/runtime"
 
 function App() {
   const [maxVolume, setMaxVolume] = useState('');
@@ -10,17 +10,24 @@ function App() {
   const [file, setFile] = useState('');
   const [normalizedFile, setNormalized] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [stream, setStream] = useState("");
+  const [stdout, setStdout] = useState([""]);
+
 
   function getVideoFile() {
+    clear()
     Videofile().then((v) => {
       setFile(v)
     })
   }
 
   function getVolume(filepath: string) {
+    EventsOn("ffmpeg", ffmpeg)
     Volume(filepath).then((v) => {
       setMaxVolume(v.Max)
       setMeanVolume(v.Mean)
+    }).finally(() => {
+      EventsOff("ffmpeg")
     })
   }
 
@@ -30,31 +37,51 @@ function App() {
     setMeanVolume("-")
     setIsLoading(false)
     setNormalized("")
+    setStream("")
+    setStdout([""])
+  }
+
+  function ffmpeg(data: Array<string>) {
+    setStdout(data)
+    // If there is a full set of data, drop the streamed data
+    setStream("")
+  }
+
+  function handleStream(data: string) {
+    setStream(data)
   }
 
   function normalize(filepath: string) {
     setIsLoading(true)
+    EventsOn("ffmpeg", ffmpeg)
+    EventsOn("stream", handleStream)
     Normalize(filepath)
       .then((result) => {
-        console.log(result)
         setNormalized(result.Outfile)
         setMaxVolume(result.Max)
         setMeanVolume(result.Mean)
       })
       .finally(() => {
+        EventsOff("ffmpeg")
+        EventsOff("stream")
         setIsLoading(false)
       })
   }
 
+
+
   return (
-    <Box height={"100vh"} bg="brand.bg">
+    <Box height={"100vh"}>
       <Banner />
-      {isLoading ? <Progress height="5px" mt="5px" mb="5px" isIndeterminate color="brand.200" /> : <Divider height="5px" mt="5px" mb="5px" />}
+      <LinearProgress loading={isLoading} />
+
       <Box display="flex" justifyContent="center" width={"100vw"}>
         <Stack width={"70vw"}>
           <Stack direction={"row"} justify="space-between">
-            <Button bgColor={"brand.300"} width="140px" onClick={getVideoFile}>Select video file</Button>
-            <Button bgColor={"brand.300"} width="140px" onClick={clear}>Clear</Button>
+            <Button disabled={isLoading} bgColor={"brand.300"} width="140px" onClick={getVideoFile}>Select video file</Button>
+            <Button disabled={isLoading || !file} width="140px" onClick={(_) => getVolume(file)}>Analyze</Button>
+            <Button disabled={isLoading || !file} width="140px" onClick={(_) => normalize(file)}>Normalize Audio</Button>
+            <Button disabled={isLoading} bgColor={"brand.300"} width="140px" onClick={clear}>Clear</Button>
           </Stack>
           <Text textColor="brand.white">In:  {file}</Text>
           <Text textColor="brand.white">Out: {normalizedFile}</Text>
@@ -62,8 +89,6 @@ function App() {
             file
               ? <>
                 <Stack direction={"row"} justify="space-between">
-                  <Button width="140px" onClick={(_) => getVolume(file)}>Analyze</Button>
-                  <Button width="140px" onClick={(_) => normalize(file)}>Normalize Audio</Button>
                 </Stack>
                 <Stack direction={"row"}>
                   <Text textColor={'brand.white'}>max volume:</Text>
@@ -73,10 +98,11 @@ function App() {
                   <Text textColor={'brand.white'}>mean volume:</Text>
                   <Text textColor={'brand.400'}>{meanVolume}</Text>
                 </Stack>
-
+                <Terminal loading={isLoading} stdout={stdout} stream={stream} />
               </>
               : null
           }
+
         </Stack>
       </Box>
     </Box >
@@ -89,6 +115,52 @@ const Banner = () => {
   return (
     <Box display="flex" bg="brand.bg" justifyContent="center">
       <Image src={banner} height="50px" mt="15px" />
+    </Box>
+  )
+}
+
+const LinearProgress = ({ loading }: { loading: boolean }) => {
+  if (loading) {
+    return <Progress height="5px" mt="5px" mb="5px" isIndeterminate color="brand.200" />
+  } else {
+    return <Divider height="5px" mt="5px" mb="5px" />
+  }
+}
+
+interface ITerminal {
+  loading: boolean
+  stdout: string[]
+  stream: string
+}
+
+const Terminal: React.FC<ITerminal> = ({ loading, stdout, stream }) => {
+
+  const [openTerminal, setOpenTerminal] = useState(false);
+
+  function showTerminal() {
+    setOpenTerminal(!openTerminal)
+  }
+
+  return (
+    <Box bg='brand.terminal' w='100%' p={4} rounded="md" >
+      <Box display="flex">
+        {stream ? <Text as="kbd" textColor="brand.white">{stream}</Text> : null}
+        {stdout && !loading ? <Button width="100px" onClick={showTerminal}>{openTerminal ? "Hide" : "Show"}</Button> : null}
+      </Box>
+      <Collapse in={openTerminal} >
+        <Stack>
+          {
+            stdout && !stream ? stdout.map(
+              (element: string, index: number) => {
+                return (
+                  <Text fontSize="13px" key={index} as="kbd" textColor="brand.white">{element}</Text>
+                )
+              }
+            )
+              : null
+          }
+        </Stack>
+      </Collapse>
     </Box>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, StyleFunctionProps } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
 
 const colors = {
   brand: {
     bg: "#303030",
     white: "#fbf5f4",
+    terminal: "#181a1f",
     200: "#f5c5b6",
     300: "#ef9578",
     400: "#ea653a",
   },
 }
 
-const theme = extendTheme({ colors })
+
+const theme = extendTheme(
+  {
+    colors,
+    styles: {
+      global: (props: StyleFunctionProps) => ({
+        body: {
+          color: 'default',
+          bg: 'brand.bg',
+          overflowX: 'hidden',
+        },
+      })
+    }
+  }
+)
 
 const container = document.getElementById('root')
 
